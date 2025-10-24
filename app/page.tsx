91079a3766c023ae9c2dc9c1ffb89b0dae7ee288
Trougier <theo.rgr2404@gmail.com>
2025-10-24 14:09:12 +0200

    test action

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import { useEffect } from "react";
 import Link from "next/link";
 
@@ -38,7 +39,7 @@ export default function Page() {
           Aller dans la Gallery
         </Link>
         <Link href="/reception" style={styles.link}>
-          Aller dans la Reception
+          Aller dans la Reception.
         </Link>
       </div>
     </div>
